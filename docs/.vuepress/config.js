b9828dbae690009cb00519278e80c5816413cc45
Kupars <59685042+Kupars@users.noreply.github.com>
2023-10-04 18:16:44 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -81,14 +81,14 @@ module.exports = {
       // 键名是该语言所属的子路径
       // 作为特例，默认语言可以使用 '/' 作为其路径。
       '/': {
-        lang: 'en-US', // 将会被设置为 <html> 的 lang 属性
-        title: 'VuePress',
-        description: 'Vue-powered Static Site Generator'
+        lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
+        title: 'OWL Library',
+        description: 'Welcome to OWL Library.'
       },
-      '/zh/': {
-        lang: 'zh-CN',
-        title: 'VuePress',
-        description: 'Vue 驱动的静态网站生成器'
+      '/en/': {
+        lang: 'en-US',
+        title: 'OWL Library',
+        description: 'Welcome to OWL Library.'
       }
     }
   }
