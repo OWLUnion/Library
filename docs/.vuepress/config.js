9ddb5ced55fd40491d0d02a4c5925c0e4c6299af
Kupars <59685042+Kupars@users.noreply.github.com>
2023-10-04 18:18:34 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -76,19 +76,3 @@ export default {
     copyCodePlugin({})
   ]
 }
-module.exports = {
-    locales: {
-      // 键名是该语言所属的子路径
-      // 作为特例，默认语言可以使用 '/' 作为其路径。
-      '/': {
-        lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
-        title: 'OWL Library',
-        description: 'Welcome to OWL Library.'
-      },
-      '/en/': {
-        lang: 'en-US',
-        title: 'OWL Library',
-        description: 'Welcome to OWL Library.'
-      }
-    }
-  }
