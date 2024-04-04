a0f1c8fd3b3b5333173cf21bf3441a01f7c812d8
AlexXuCN <alexxucn0@outlook.com>
2024-04-05 00:27:33 +0800

    重整了package.json和config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,16 @@
-import { gitPlugin } from "@vuepress/plugin-git";
-import { mdEnhancePlugin } from "vuepress-plugin-md-enhance";
-import { copyCodePlugin } from "vuepress-plugin-copy-code2";
-import { defaultTheme } from '@vuepress/theme-default';
+import { defaultTheme } from '@vuepress/theme-default'
+import { viteBundler } from '@vuepress/bundler-vite'
+import { gitPlugin } from '@vuepress/plugin-git'
+import { mdEnhancePlugin } from 'vuepress-plugin-md-enhance'
+import { copyCodePlugin } from '@vuepress/plugin-copy-code'
+import { docsearchPlugin } from '@vuepress/plugin-docsearch'
 
-import financialDetailsPlugin from "./utils/financialDetails.js"
-import autoSidebar from "./utils/autoSidebar.js"
+import financialDetailsPlugin from './utils/financialDetails.js'
+import autoSidebar from './utils/autoSidebar.js'
 
 
 export default {
+    bundler: viteBundler(),
     locales: {
         '/': {
             lang: 'zh-CN',
@@ -41,13 +44,13 @@ export default {
                         text: 'Wiki',
                         link: '/wiki/',
                         collapsible: true,
-                        children: autoSidebar("wiki")
+                        children: autoSidebar('wiki')
                     },
                     {
                         text: '公告',
                         link: '/announcement/',
                         collapsible: true,
-                        children: autoSidebar("announcement")
+                        children: autoSidebar('announcement')
                     },
                     {
                         text: '资金明细',
@@ -58,7 +61,7 @@ export default {
                         text: '公约',
                         link: '/rules/',
                         collapsible: true,
-                        children: autoSidebar("rules")
+                        children: autoSidebar('rules')
                     },
                     {
                         text: '加入服务器',
@@ -75,7 +78,7 @@ export default {
             '/en/': {
                 selectLanguageName: 'English',
                 selectLanguageText: 'Languages',
-                selectLanguageAriaLabel: "Languages",
+                selectLanguageAriaLabel: 'Languages',
                 editLinkText: 'Edit on GitHub',
                 repoLabel: 'View on Github',
                 lastUpdatedText: 'Last Updated',
@@ -85,13 +88,13 @@ export default {
                         text: 'Wiki',
                         link: '/en/wiki/',
                         collapsible: true,
-                        children: autoSidebar("en/wiki")
+                        children: autoSidebar('en/wiki')
                     },
                     {
                         text: 'Announcements',
                         link: '/en/announcement/',
                         collapsible: true,
-                        children: autoSidebar("en/announcement")
+                        children: autoSidebar('en/announcement')
                     },
                     {
                         text: 'Finance Details',
@@ -102,7 +105,7 @@ export default {
                         text: 'Rules',
                         link: '/en/rules/',
                         collapsible: true,
-                        children: autoSidebar("en/rules")
+                        children: autoSidebar('en/rules')
                     },
                     {
                         text: 'Join Server',
@@ -142,34 +145,40 @@ export default {
             components: true
         }),
         copyCodePlugin({}),
+        docsearchPlugin({
+            apiKey: '1c9f43ca88682ced733c7ddc35ef672a',
+            indexName: 'lib-kupars2',
+            appid: 'JY2N18S6T2',
+            placeholder: 'Search'
+        }),
         financialDetailsPlugin({
             locales: {
-                "/": {
-                    template: "/.vuepress/financialDetails/zh_cn.md",
-                    path: "/finance/",
-                    chapterTitle: "### 玩家赞助明细",
-                    summary: ["我们共收到了 ", " 元赞助。"],
-                    month: "月",
-                    year: "年",
-                    day: "日",
-                    playerName: "玩家名称",
-                    amount: "赞助金额 (CNY)",
-                    date: "赞助日期"
+                '/': {
+                    template: '/.vuepress/financialDetails/zh_cn.md',
+                    path: '/finance/',
+                    chapterTitle: '### 玩家赞助明细',
+                    summary: ['我们共收到了 ', ' 元赞助。'],
+                    month: '月',
+                    year: '年',
+                    day: '日',
+                    playerName: '玩家名称',
+                    amount: '赞助金额 (CNY)',
+                    date: '赞助日期'
                 },
-                "/en/": {
-                    template: "/.vuepress/financialDetails/en_us.md",
-                    path: "/en/finance",
-                    chapterTitle: "### Sponsorship Details",
-                    summary: ["We received a total of ¥", " (CNY) in sponsorship."],
-                    month: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-                    year: "",
-                    day: "",
-                    playerName: "Sponsor",
-                    amount: "Amount (CNY)",
-                    date: "Day of the Month"
+                '/en/': {
+                    template: '/.vuepress/financialDetails/en_us.md',
+                    path: '/en/finance',
+                    chapterTitle: '### Sponsorship Details',
+                    summary: ['We received a total of ¥', ' (CNY) in sponsorship.'],
+                    month: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                    year: '',
+                    day: '',
+                    playerName: 'Sponsor',
+                    amount: 'Amount (CNY)',
+                    date: 'Day of the Month'
                 }
             },
-            data: "/.vuepress/financialDetails/data.toml"
+            data: '/.vuepress/financialDetails/data.toml'
         })
     ]
 }
